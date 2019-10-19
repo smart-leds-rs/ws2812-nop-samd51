7eb5390f8f46325d37048a83366caf13f7f43a36
Jacob <jacobrosenthal@Jacobs-MacBook-Air.local>
2019-10-19 11:27:48 -0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
 use cortex_m::asm::delay;
-use embedded_hal::digital::OutputPin;
-use smart_leds_trait::{Color, SmartLedsWrite};
+use embedded_hal::digital::v2::OutputPin;
+use smart_leds_trait::{SmartLedsWrite, RGB8};
 
 pub struct Ws2812<P: OutputPin> {
     pin: P,
@@ -15,14 +15,14 @@ impl<P: OutputPin> Ws2812<P> {
     fn write_byte(&mut self, data: u8) {
         let mut bitmask: u8 = 0x80;
         while bitmask != 0 {
-            self.pin.set_high();
+            self.pin.set_high().unwrap_or(());
             delay(2);
             if data & bitmask != 0 {
-                delay(3); 
-                self.pin.set_low();
+                delay(3);
+                self.pin.set_low().unwrap_or(());
             } else {
-                self.pin.set_low();
-                delay(2); 
+                self.pin.set_low().unwrap_or(());
+                delay(2);
             }
             bitmask >>= 1;
             delay(4);
@@ -35,15 +35,18 @@ impl<P> SmartLedsWrite for Ws2812<P>
 where
     P: OutputPin,
 {
+    type Color = RGB8;
     type Error = ();
-    fn write<T>(&mut self, iterator: T) -> Result<(), ()>
+    fn write<T, I>(&mut self, iterator: T) -> Result<(), ()>
     where
-        T: Iterator<Item = Color>,
+        T: Iterator<Item = I>,
+        I: Into<Self::Color>,
     {
         for item in iterator {
-            self.write_byte(item.g);
-            self.write_byte(item.r);
-            self.write_byte(item.b);
+            let color: Self::Color = item.into();
+            self.write_byte(color.g);
+            self.write_byte(color.r);
+            self.write_byte(color.b);
         }
         Ok(())
     }
